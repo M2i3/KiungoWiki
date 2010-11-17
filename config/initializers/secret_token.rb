536710c5ae4d2773f8e10e954b25a620748558f1
Jean-Marc Lagacé <jean-marc.lagace@m2i3.com>
2010-11-17 07:19:23 -0500

    cadre applicatif vide avec les références aux bonnes librairies

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+KiungoWiki::Application.config.secret_token = '46042a179751327af7f99ae5d9cac3ee26ef614ede2cd572aca708916da456688ab58faf9d086e73bcdb709e7d381065d2f7c9715fa45b72c0508e29c4dd6a15'
