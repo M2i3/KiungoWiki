ad54e1e3130f341aa56323df67344ca764947a36
Jean-Marc Lagacé <jean-marc.lagace@m2i3.com>
2011-07-06 08:31:39 -0400

    from Pierre's comment: duration and rythm can be unknown in cases when we learn there is a recording but know very little about it

--- app/models/recording.rb
+++ app/models/recording.rb
@@ -1,13 +1,22 @@
 class Recording
   include Mongoid::Document
-  field :recording_date, :type => DateTime
+  include Mongoid::Timestamps
+  include Mongoid::Versioning
+
+  field :work_name, :type => String
+  field :recording_date, :type => DateTime #VarPrecisionDate
   field :recording_location, :type => String
   field :duration, :type => Integer
   field :rythm, :type => Integer
+
+  field :updated_by, :default => "anonymous"
   
-  validates_presence_of :duration
-  validates_numericality_of :duration, :greater_than=>0
-  
+
+  validates_numericality_of :duration, :greater_than=>0, :allow_nil=>true  
   validates_numericality_of :rythm, :greater_than=>0, :allow_nil=>true
+
+  def title
+    self.work_name
+  end
 
 end
--- spec/models/recording_spec.rb
+++ spec/models/recording_spec.rb
@@ -3,14 +3,14 @@ require 'spec_helper'
 describe Recording do
 
 	describe "Testing for required values" do 
-		[:recording_date, :recording_location, :rythm].each {|field|	
+		[:recording_date, :recording_location, :rythm, :duration].each {|field|	
 			it "#{field} should allow nil" do 
 				rec = Factory.build(:recording, field =>  nil)			
 				rec.valid?.should == true
 			end
 		}
 	
-		[:duration].each {|field|	
+		[].each {|field|	
 			it "#{field} should not allow nil" do 
 				rec = Factory.build(:recording, field =>  nil)			
 				rec.valid?.should == false
@@ -20,7 +20,7 @@ describe Recording do
 	end
 	
 	describe "Testing for the duration" do	
-		["a", nil, -15, 0].each {|value|
+		["a", -15, 0].each {|value|
 			it "when specified, duration should be numerical and greater than 0 (#{value} is not valid)" do
 				rec = Factory.build(:recording, :duration=>value)
 				rec.valid?.should == false
