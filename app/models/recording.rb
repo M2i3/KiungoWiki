823f473bfbaa4dc6481646982fb0cd31011a8f6d
Hugues Lamy <hugues.lamy@m2i3.com>
2011-07-14 13:15:33 -0400

    Variable date IncDate working

--- app/models/recording.rb
+++ app/models/recording.rb
@@ -4,15 +4,13 @@ class Recording
   include Mongoid::Versioning
 
   field :work_name, :type => String
-  field :recording_date, :type => DateTime #VarPrecisionDate
+  field :recording_date, :type => IncDate
   field :recording_location, :type => String
   field :duration, :type => Integer
   field :rythm, :type => Integer
   field :sample_rate, :type => Integer
-
   field :updated_by, :default => "anonymous"
   
-
   validates_length_of :work_name, :in=>1..500, :allow_nil=>true
   validates_numericality_of :duration, :greater_than=>0, :allow_nil=>true  
   validates_numericality_of :rythm, :greater_than=>0, :allow_nil=>true
