da29d014c2f930c3d240690c443a03cedb01dab0
Jean-Marc Lagacé <jean-marc.lagace@m2i3.com>
2011-09-13 16:55:45 -0400

    disabling usage of the CDN on the staging app - test is completed

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -32,7 +32,7 @@ KiungoWiki::Application.configure do
   config.serve_static_assets = true
 
   # Enable serving of images, stylesheets, and javascripts from an asset server
-  config.action_controller.asset_host = "http://d3vnx7plwzifdg.cloudfront.net"
+  # config.action_controller.asset_host = "http://d3vnx7plwzifdg.cloudfront.net"
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
