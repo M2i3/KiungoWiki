2d37484abae27c51ef7cfffa97d0b93ebc617446
Jean-Marc Lagacé <jean-marc.lagace@m2i3.com>
2011-09-26 22:13:06 -0400

    implementing search queries with their tests on the artist and work

--- app/models/artist_search_query.rb
+++ app/models/artist_search_query.rb
@@ -1,11 +1,11 @@
 class ArtistSearchQuery < SearchQuery 
   def self.query_expressions
-    { name: / name:"(.+)" /, 
+    superclass.query_expressions.merge({name: / name:"(.+?)" /, 
       birth_date: / birth_date:([0-9]{4,4}(\-|\/)[0-9]{1,2}(\-|\/)[0-9]{1,2}) /,
-      birth_location: / birth_location:"(.+)" /,
+      birth_location: / birth_location:"(.+?)" /,
       death_date: / death_date:([0-9]{4,4}(\-|\/)[0-9]{1,2}(\-|\/)[0-9]{1,2}) /,
-      death_location: / death_location:"(.+)" /
-    }
+      death_location: / death_location:"(.+?)" /
+    })
   end
   def self.catch_all
     "name"
