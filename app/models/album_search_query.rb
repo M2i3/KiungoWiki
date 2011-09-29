8e8a18f5a4b0cf213866aae092f2afaa2679c1aa
Jean-Marc Lagacé <jean-marc.lagace@m2i3.com>
2011-09-28 23:09:53 -0400

    implemented the query parsing for the Recording

--- app/models/album_search_query.rb
+++ app/models/album_search_query.rb
@@ -1,10 +1,10 @@
 class AlbumSearchQuery < SearchQuery 
   def self.query_expressions
-    superclass.query_expressions.merge({ title: / title:"(.+)" /,
-      media_type: / media_type:"(.+)" /,
+    superclass.query_expressions.merge({ title: / title:"(.+?)" /,
+      media_type: / media_type:"(.+?)" /,
       date_released: / date_released:([0-9]{4,4}(\-|\/)[0-9]{1,2}(\-|\/)[0-9]{1,2}) /,
-      label: / label:"(.+)" /,
-      reference_number: / reference_code:"(.+)" /
+      label: / label:"(.+?)" /,
+      reference_code: / reference_code:"(.+?)" /
     })
   end
   def self.catch_all
---
+++ spec/models/album_search_query_spec.rb
@@ -0,0 +1,23 @@
+require 'spec_helper'
+
+describe AlbumSearchQuery do
+  it "should all go under the title when doing a basic query" do
+    wsq = AlbumSearchQuery.new('This is a search " query with no keyworkds"')
+    wsq[:title].should eql('This is a search " query with no keyworkds"')
+    wsq[:media_type].should eql(nil)
+    wsq[:label].should eql(nil)
+    wsq[:reference_code].should eql(nil)
+    wsq[:oid].should eql(nil)
+  end
+
+  it "should set all the attributes in a complete query using what's left for the title" do
+    wsq = AlbumSearchQuery.new('date_released:2011-09-01 This is a search " query with no keyworkds" label:"Production Pierre F." oid:4e81265241c25e24b6000001 reference_code:"425345lkj-1234"')
+    wsq[:title].should eql('This is a search " query with no keyworkds"')
+    wsq[:date_released].should eql("2011-09-01")
+    wsq[:label].should eql("Production Pierre F.")
+    wsq[:reference_code].should eql("425345lkj-1234")
+    wsq[:oid].should eql("4e81265241c25e24b6000001")
+  end
+end
+
+
