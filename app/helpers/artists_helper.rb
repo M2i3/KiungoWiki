e079702999df09aa75a7fb533344f1f6b53c285d
Shawn <shawninder@gmail.com>
2011-09-29 03:12:37 -0400

    Internationalizing + a few french translations

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -4,4 +4,12 @@ module ArtistsHelper
       content_tag(:dt, label) + content_tag(:dd, [date, location].reject{|v| v.blank?}.join(", "))      
     end
   end
+  
+  def updated_or_created_formatted(modified_label, created_label, updated_at, created_at)
+  	if updated_at.blank?
+  		content_tag(:small, updated_label + " " + updated_at.to_s)
+  	else
+  		content_tag(:small, created_label + " " + created_at.to_s)
+  	end
+  end
 end
