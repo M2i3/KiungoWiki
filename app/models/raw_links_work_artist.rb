282ab3cd0b825266af33a4bfa67ba56ee153c23b
Michel Mayrand <mmayrand@videotron.ca>
2011-10-13 07:35:27 -0400

    oublié dans le dernier push

---
+++ app/models/raw_links_work_artist.rb
@@ -0,0 +1,14 @@
+class RawLinksWorkArtist
+  include Mongoid::Document
+
+  set_database :raw_db
+#TODO: Re-enable some form of versioning most likely using https://github.com/aq1018/mongoid-history instead of the Mongoid::Versioning module
+  #after_initialize :set_defaults
+
+  field :work_id, :type => String
+  field :artist_id, :type => String
+
+  private 
+  def set_defaults
+  end
+end
