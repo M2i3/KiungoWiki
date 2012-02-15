a73e5da9bbcd3aeb057e527698b30963a68fcb2a
Shawn <shawninder@gmail.com>
2012-02-15 11:08:27 -0500

    Remove need for quotes to search in a specific field (like given_name for example)

--- app/models/artist_search_query.rb
+++ app/models/artist_search_query.rb
@@ -1,12 +1,12 @@
 class ArtistSearchQuery < SearchQuery 
   def self.query_expressions
-    superclass.query_expressions.merge({surname: / surname:[ "]*(.+?)[ "]* /,
-      given_name: / given_name:[ "]*(.+?)[ "]* /, 
-      name: / name:[ "]*(.+?)[ "]* /,
-      birth_date: / birth_date:[ "]*(.+?)[ "]* /,
-      birth_location: / birth_location:[ "]*(.+?)[ "]* /,
-      death_date: / death_date:[ "]*(.+?)[ "]* /,
-      death_location: / death_location:[ "]*(.+?)[ "]* /,
+    superclass.query_expressions.merge({surname: / surname:(.+?) /,
+      given_name: / given_name:(.+?) /, 
+      name: / name:(.+?) /,
+      birth_date: / birth_date:(.+?) /,
+      birth_location: / birth_location:(.+?) /,
+      death_date: / death_date:(.+?) /,
+      death_location: / death_location:(.+?) /,
       role: / role:([0-9a-zA-Z]+) /
     })
   end
