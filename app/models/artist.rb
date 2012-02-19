783825f435c8151cc8bfb4a1033da67a34f0e7e7
Michel Mayrand <mmayrand@videotron.ca>
2012-02-19 10:32:10 -0500

    typo corrected for birth_date

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -90,7 +90,7 @@ class Artist
       case field
         when :name, :surname, :given_name, :birth_location, :death_location
           current_query = current_query.where(field=>/#{asq[field].downcase}/i)
-        when :birth_death, :death_date, :created_at, :updated_at
+        when :birth_date, :death_date, :created_at, :updated_at
           current_query = current_query.where(field=>asq[field])        
       end 
     }
