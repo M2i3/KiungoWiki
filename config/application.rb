1a53b5521bfe32d582ecc78aeeb0e00376789e78
Jean-Marc Lagacé <jean-marc.lagace@m2i3.com>
2012-03-17 15:15:22 -0400

    took advantage of asset pipelining or Rails3

--- config/application.rb
+++ config/application.rb
@@ -48,7 +48,9 @@ module KiungoWiki
     config.filter_parameters += [:password]
 
     # Enable the asset pipeline
-    config.assets.enabled = false
+    config.assets.enabled = true
+    config.assets.initialize_on_precompile = false
+    config.assets.js_compressor = :yui
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
