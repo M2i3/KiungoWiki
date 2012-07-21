18b3acd34108e7895b1a77da8f7096daeb739a87
Jean-Marc Lagacé <jean-marc.lagace@m2i3.com>
2012-07-21 14:46:27 -0400

    streamlined building the lookup as work_wiki_link

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -80,12 +80,15 @@ class WorksController < ApplicationController
   end
 
   def lookup
+
+    wsq = WorkSearchQuery.new(params[:q])
+
     respond_to do |format|
       format.json { 
-        render :json=>(Work.queried(params[:q]).limit(20).collect{|w| 
-          reference_text = ["oid:#{w.id}"]
-          reference_label = [w.title]
-          {id: reference_text.join(" "), name: reference_label.join(" ")}               
+        render :json=>(Work.queried(wsq.objectq).limit(20).collect{|w| 
+
+          WorkWikiLink.new(reference_text: "oid:#{w.id} #{wsq.metaq}").combined_link
+
          } << {id: params[:q].to_s, name: params[:q].to_s + " (nouveau)"}) 
        }
     end
