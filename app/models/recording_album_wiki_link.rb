a0bab6571e2f3ce210cdd74e3ab87773c10c7cb3
Jean-Marc Lagacé <jean-marc.lagace@m2i3.com>
2012-09-24 08:25:50 -0400

    linking the recording and album using context specific wiki link

---
+++ app/models/recording_album_wiki_link.rb
@@ -0,0 +1,30 @@
+class RecordingAlbumWikiLink < AlbumWikiLink
+  include Mongoid::Document
+
+  def trackNb
+    searchref[:trackNb]
+  end
+
+  def itemId
+    searchref[:itemId]
+  end
+
+  def itemSection
+    searchref[:itemSection]
+  end
+
+  class SearchQuery < self.superclass::SearchQuery 
+    def self.query_expressions
+      super.merge({ 
+        trackNb: :numeric,
+        itemId: :word,
+        itemSection: :character      
+      })
+    end
+    def self.meta_fields
+      super + [:trackNb, :itemId, :itemSection]
+    end
+  end
+end
+
+
