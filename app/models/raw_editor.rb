6e25bfd27083bb44e5632459b4bbe289d73f994d
Jean-Marc Lagacé <jean-marc.lagace@m2i3.com>
2013-03-11 20:29:03 -0400

    disabling set_database on raw models to allow running on heroku.

--- app/models/raw_editor.rb
+++ app/models/raw_editor.rb
@@ -2,7 +2,7 @@ class RawEditor
   include Mongoid::Document
   include Mongoid::Timestamps
 
-  set_database :raw_db
+#  set_database :raw_db
 #TODO: Re-enable some form of versioning most likely using https://github.com/aq1018/mongoid-history instead of the Mongoid::Versioning module
   #after_initialize :set_defaults
 
--- app/models/raw_recording_artist_role_link.rb
+++ app/models/raw_recording_artist_role_link.rb
@@ -1,7 +1,7 @@
 class RawRecordingArtistRoleLink
   include Mongoid::Document
 
-  set_database :raw_db
+#  set_database :raw_db
 #TODO: Re-enable some form of versioning most likely using https://github.com/aq1018/mongoid-history instead of the Mongoid::Versioning module
   #after_initialize :set_defaults
 
