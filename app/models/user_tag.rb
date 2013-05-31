731b620b79be0291d1674eacefad8b58ce7b66cc
Allen Wyma <allen.wyma@gmail.com>
2013-05-31 22:26:52 +0800

    tried to test creation of public tags, but not working out

--- app/models/user_tag.rb
+++ app/models/user_tag.rb
@@ -11,4 +11,21 @@ class UserTag
   def taggable
     self.taggable_type.constantize.find self.taggable_id
   end
+  
+  after_create do |doc|
+    # if no tag is present that is relevent
+    if doc.taggable.class.where("tags.name" => /#{doc.name}/i, id:doc.taggable.id).size == 0
+      count = ENV["TAG_COUNT"].to_i # environment set tag count
+      # if there is other tags like this that equal this
+      if UserTag.where(name: /#{doc.name}/i, taggable_type:doc.taggable_type, taggable_id:doc.taggable_id).size == (count - 1)
+        doc.taggable.tags << PublicTag.new(name: doc.name, size: count) # create and add a new tag
+        doc.taggable.save! # save the taggable
+      end
+    else
+      tag = doc.taggable.class.where("tags.name" => /#{doc.name}/i).first # grab first relevant tag
+      tag.size += 1 # increment the count
+      tag.save! # save the tag
+    end
+  end
+  
 end
--- spec/models/user_tag_spec.rb
+++ spec/models/user_tag_spec.rb
@@ -15,4 +15,17 @@ describe UserTag do
     Artist.should_receive(:find).with(id).and_return artist
     subject.taggable.should eq artist
   end
+  # it "should be able to search and create a public tag inside of the resource if it's at or above the threshold" do
+  #   artist = FactoryGirl.create(:artist)
+  #   user = FactoryGirl.create(:user)
+  #   name = "test"
+  #   subject.taggable = artist
+  #   subject.name = name
+  #   subject.user = user
+  #   # .with(name: /#{name}/i, taggable_type: artist.class.to_s, taggable_id:artist.id).
+  #   UserTag.should_receive(:where).at_least(1).and_return UserTag
+  #   UserTag.should_receive(:size).at_least(1).and_return (ENV["TAG_COUNT"].to_i - 1)
+  #   subject.save!
+  #   artist.reload.tags.size.should eq 1
+  # end
 end
